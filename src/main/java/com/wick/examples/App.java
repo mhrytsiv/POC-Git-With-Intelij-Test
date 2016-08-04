5ef45d6c7eb409eb9c14438376d0c4a56cb8140c
Tymur_Hrytsiv <Tymur_Hrytsiv@epam.com>
2016-08-04 17:02:06 +0300

    TH: Changed App

--- src/main/java/com/wick/examples/App.java
+++ src/main/java/com/wick/examples/App.java
@@ -3,6 +3,6 @@ package com.wick.examples;
 public class App {
 
     public static void main(final String... strings) {
-        System.out.println("Inside main");
+        System.out.println("Changed by Tymur");
     }
 }
