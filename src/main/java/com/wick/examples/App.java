91287278cffaaac9303a02aa72d8ba77aee0f06b
Tymur_Hrytsiv <Tymur_Hrytsiv@epam.com>
2016-08-04 18:08:00 +0300

    TH: Changed App 3.

--- src/main/java/com/wick/examples/App.java
+++ src/main/java/com/wick/examples/App.java
@@ -3,6 +3,6 @@ package com.wick.examples;
 public class App {
 
     public static void main(final String... strings) {
-        System.out.println("Changed 2 by MH in develop branch");
+        System.out.println("Changed 3 by TH in develop branch");
     }
 }
